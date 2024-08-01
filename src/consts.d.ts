c3a63cd0c45e571128f02fc80c7636e7d2dcfd98
Eckry <112786854+Eckry@users.noreply.github.com>
2024-08-01 17:58:36 -0600

    add skills

--- src/consts.d.ts
+++ src/consts.d.ts
@@ -1,18 +1,23 @@
 export const projects = [
   {
-    "name": "E-commerce",
-    "image": "https://vitejs.dev/logo-with-shadow.png",
-    "description": "Pariatur elit occaecat eu sunt ea incididunt amet Lorem occaecat elit et amet pariatur sit. Proident Lorem duis dolore voluptate commodo. Velit et ex nulla labore aliquip ut eu qui dolore in aliqua ad. Mollit dolore et incididunt labore consectetur culpa esse incididunt consequat. Deserunt esse ullamco qui cillum sint magna consequat aliqua in amet.",
-    "tech": ["ts", "react"],
-    "link": ""
+    name: "E-commerce",
+    image: "https://vitejs.dev/logo-with-shadow.png",
+    description:
+      "Pariatur elit occaecat eu sunt ea incididunt amet Lorem occaecat elit et amet pariatur sit. Proident Lorem duis dolore voluptate commodo. Velit et ex nulla labore aliquip ut eu qui dolore in aliqua ad. Mollit dolore et incididunt labore consectetur culpa esse incididunt consequat. Deserunt esse ullamco qui cillum sint magna consequat aliqua in amet.",
+    tech: ["ts", "react"],
+    link: "",
   },
   {
-    "name": "Sorted Symphony",
-    "image": "https://vitejs.dev/logo-with-shadow.png",
-    "description": "Pariatur elit occaecat eu sunt ea incididunt amet Lorem occaecat elit et amet pariatur sit. Proident Lorem duis dolore voluptate commodo. Velit et ex nulla labore aliquip ut eu qui dolore in aliqua ad. Mollit dolore et incididunt labore consectetur culpa esse incididunt consequat. Deserunt esse ullamco qui cillum sint magna consequat aliqua in amet.",
-    "tech": ["ts", "react"],
-    "link": ""
-  }
-]
+    name: "Sorted Symphony",
+    image: "https://vitejs.dev/logo-with-shadow.png",
+    description:
+      "Pariatur elit occaecat eu sunt ea incididunt amet Lorem occaecat elit et amet pariatur sit. Proident Lorem duis dolore voluptate commodo. Velit et ex nulla labore aliquip ut eu qui dolore in aliqua ad. Mollit dolore et incididunt labore consectetur culpa esse incididunt consequat. Deserunt esse ullamco qui cillum sint magna consequat aliqua in amet.",
+    tech: ["ts", "react"],
+    link: "",
+  },
+];
 
-export const skills = [] as const
+export const skills = {
+  backend: [{ name: "Express", icon: "" }],
+  frontend: [{ name: "React", icon: "" }],
+};
