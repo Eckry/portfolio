e9104f289bf9c7a608d9bd9034a79278dbca3776
Eckry <112786854+Eckry@users.noreply.github.com>
2024-08-01 17:46:31 -0600

    add flex to main container

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Presentation from "./components/Presentation";
+import ProjectsSection from "./components/ProjectsSection";
 
 function App() {
   return (
     <main className="main-container">
       <Presentation />
+      <ProjectsSection />
     </main>
   );
 }
