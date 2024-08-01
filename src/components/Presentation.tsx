edba8bf7116144748c9c40a5ab1c206deb11ef95
Eckry <112786854+Eckry@users.noreply.github.com>
2024-08-01 17:09:59 -0600

    create Presentation component

---
+++ src/components/Presentation.tsx
@@ -0,0 +1,26 @@
+export default function Presentation() {
+  return (
+    <section>
+      <div>
+        <div>
+          <h1>Hey! I'm Erick Torres</h1>
+          <h2>I'm a software developer</h2>
+          <p>A passionate physics engineer that found his love for coding!</p>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <a href="https://github.com/Eckry" target="_blank">
+                Github
+              </a>
+            </li>
+            <li>
+              <a href="">Linkdin</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <img src="https://vitejs.dev/logo-with-shadow.png" alt="" />
+    </section>
+  );
+}
