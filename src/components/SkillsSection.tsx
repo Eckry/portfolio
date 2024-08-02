cade41e747a812afa535e3824ea896cc3934cf46
Eckry <112786854+Eckry@users.noreply.github.com>
2024-08-01 18:04:23 -0600

    create a section-title standar

--- src/components/SkillsSection.tsx
+++ src/components/SkillsSection.tsx
@@ -5,18 +5,21 @@ import "./styles/SkillsSection.css";
 export default function SkillsSection() {
   return (
     <section className="skills-container">
-      <article className="skills-article">
-        <h2>Frontend</h2>
-        {skills.backend.map((skill) => {
-          return <Skill {...skill} />;
-        })}
-      </article>
-      <article className="skills-article">
-        <h2>Backend</h2>
-        {skills.frontend.map((skill) => {
-          return <Skill {...skill} />;
-        })}
-      </article>
+      <h1 className="section-title">Skills</h1>
+      <div className="skills-wrapper">
+        <article className="skills-article">
+          <h2>Frontend</h2>
+          {skills.backend.map((skill) => {
+            return <Skill {...skill} />;
+          })}
+        </article>
+        <article className="skills-article">
+          <h2>Backend</h2>
+          {skills.frontend.map((skill) => {
+            return <Skill {...skill} />;
+          })}
+        </article>
+      </div>
     </section>
   );
 }
