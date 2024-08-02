bee7f95c1e8994f1bbeab95d39f7e2cd8d2946a7
Eckry <112786854+Eckry@users.noreply.github.com>
2024-08-01 18:01:24 -0600

    import SkillsSection

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Presentation from "./components/Presentation";
 import ProjectsSection from "./components/ProjectsSection";
+import SkillsSection from "./components/SkillsSection";
 
 function App() {
   return (
     <main className="main-container">
       <Presentation />
       <ProjectsSection />
+      <SkillsSection />
     </main>
   );
 }
