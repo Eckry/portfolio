e8c51c3599cb578dcc270e774b03d350879c32da
Eckry <112786854+Eckry@users.noreply.github.com>
2024-08-02 19:29:14 -0600

    add contact icon

--- src/icons.tsx
+++ src/icons.tsx
@@ -108,3 +108,22 @@ export function IconSkills() {
     </svg>
   );
 }
+
+export function IconContact() {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z" />
+      <path d="M3 7l9 6l9 -6" />
+    </svg>
+  );
+}
