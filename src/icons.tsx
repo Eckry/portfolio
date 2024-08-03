f8e944d5bf9b02ab03ee561c220178c9223b38bf
Eckry <112786854+Eckry@users.noreply.github.com>
2024-08-02 19:19:28 -0600

    add folder icon

--- src/icons.tsx
+++ src/icons.tsx
@@ -53,3 +53,21 @@ export function IconCV() {
     </svg>
   );
 }
+
+export function IconFolder() {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M5 19l2.757 -7.351a1 1 0 0 1 .936 -.649h12.307a1 1 0 0 1 .986 1.164l-.996 5.211a2 2 0 0 1 -1.964 1.625h-14.026a2 2 0 0 1 -2 -2v-11a2 2 0 0 1 2 -2h4l3 3h7a2 2 0 0 1 2 2v2" />
+    </svg>
+  );
+}
