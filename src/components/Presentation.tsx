fa184854347d7e4d1f687c6f8d15b5098185eeaa
Eckry <112786854+Eckry@users.noreply.github.com>
2024-08-02 19:07:55 -0600

    fix small things on presentation

--- src/components/Presentation.tsx
+++ src/components/Presentation.tsx
@@ -1,7 +1,15 @@
 import { social } from "../consts.d";
 import "./styles/Presentation.css";
 
-function Link({ link, name, Icon }: { link: string; name: string, Icon: () => JSX.Element }) {
+function Link({
+  link,
+  name,
+  Icon,
+}: {
+  link: string;
+  name: string;
+  Icon: () => JSX.Element;
+}) {
   return (
     <a className="presentation-link" href={link} target="_blank">
       <Icon />
@@ -13,37 +21,38 @@ function Link({ link, name, Icon }: { link: string; name: string, Icon: () => JS
 export default function Presentation() {
   return (
     <section className="presentation-container">
-      <div>
+      <header className="presentation-header">
         <div>
           <h1 className="presentation-title">Hey! I'm Erick Torres</h1>
           <h2 className="presentation-subtitle">
-            A <span className="highlight-1">software developer</span>
+            A <span className="highlight-1">software developer</span>.
           </h2>
-          <p className="presentation-p">
-            I'm a passionate physics engineer that found his love for coding!
-          </p>
         </div>
-        <nav className="presentation-nav">
-          <div className="presentation-available">
-            <span className="presentation-dot"></span>
-            <span>Available for work</span>
-          </div>
-          <ul className="presentation-ul">
-            {social.map((network) => {
-              return (
-                <li key={network.name}>
-                  <Link {...network} />
-                </li>
-              );
-            })}
-          </ul>
-        </nav>
-      </div>
-      <img
-        className="presentation-image"
-        src="https://vitejs.dev/logo-with-shadow.png"
-        alt="me"
-      />
+        <img
+          className="presentation-image"
+          src="https://vitejs.dev/logo-with-shadow.png"
+          alt="me"
+        />
+      </header>
+      <p className="presentation-p">
+        I'm a passionate physics engineer that found his love for coding!
+      </p>
+      <nav className="presentation-nav">
+        <div className="presentation-available">
+          <span className="presentation-dot"></span>
+          <span>Available for work</span>
+        </div>
+        <span className="presentation-line"></span>
+        <ul className="presentation-ul">
+          {social.map((network) => {
+            return (
+              <li key={network.name}>
+                <Link {...network} />
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
     </section>
   );
 }
