6f6c9d1c7e51d353a9e43fbf194b51c2a4808168
Eckry <112786854+Eckry@users.noreply.github.com>
2024-08-05 17:57:27 -0600

    add typescript and react icons

--- src/icons.tsx
+++ src/icons.tsx
@@ -147,3 +147,42 @@ export function IconLink() {
     </svg>
   );
 }
+
+export function IconTypeScript() {
+  return (
+    <svg
+      width="32"
+      height="32"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M15 17.5c.32 .32 .754 .5 1.207 .5h.543c.69 0 1.25 -.56 1.25 -1.25v-.25a1.5 1.5 0 0 0 -1.5 -1.5a1.5 1.5 0 0 1 -1.5 -1.5v-.25c0 -.69 .56 -1.25 1.25 -1.25h.543c.453 0 .887 .18 1.207 .5" />
+      <path d="M9 12h4" />
+      <path d="M11 12v6" />
+      <path d="M21 19v-14a2 2 0 0 0 -2 -2h-14a2 2 0 0 0 -2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2 -2z" />
+    </svg>
+  );
+}
+
+export function IconReact() {
+  return (
+    <svg
+      width="28"
+      height="28"
+      viewBox="-10.5 -9.45 21 18.9"
+      fill="none"
+    >
+      <circle cx="0" cy="0" r="2" fill="currentColor"></circle>
+      <g stroke="currentColor" stroke-width="1" fill="none">
+        <ellipse rx="10" ry="4.5"></ellipse>
+        <ellipse rx="10" ry="4.5" transform="rotate(60)"></ellipse>
+        <ellipse rx="10" ry="4.5" transform="rotate(120)"></ellipse>
+      </g>
+    </svg>
+  );
+}
