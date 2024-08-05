f6ef0896e0df302d7b76a9e2b45b982598c36939
Eckry <112786854+Eckry@users.noreply.github.com>
2024-08-05 17:35:16 -0600

    add project images

--- src/consts.d.ts
+++ src/consts.d.ts
@@ -3,7 +3,7 @@ import { IconCV, IconGithub, IconLinkedin } from "./icons";
 export const projects = [
   {
     name: "E-commerce",
-    image: "https://vitejs.dev/logo-with-shadow.png",
+    image: "ecommerce.webp",
     description:
       "Pariatur elit occaecat eu sunt ea incididunt amet Lorem occaecat elit et amet pariatur sit. Proident Lorem duis dolore voluptate commodo. Velit et ex nulla labore aliquip ut eu qui dolore in aliqua ad. Mollit dolore et incididunt labore consectetur culpa esse incididunt consequat. Deserunt esse ullamco qui cillum sint magna consequat aliqua in amet.",
     tech: ["ts", "react"],
@@ -11,7 +11,15 @@ export const projects = [
   },
   {
     name: "Sorted Symphony",
-    image: "https://vitejs.dev/logo-with-shadow.png",
+    image: "sortedsymphony.webp",
+    description:
+      "Pariatur elit occaecat eu sunt ea incididunt amet Lorem occaecat elit et amet pariatur sit. Proident Lorem duis dolore voluptate commodo. Velit et ex nulla labore aliquip ut eu qui dolore in aliqua ad. Mollit dolore et incididunt labore consectetur culpa esse incididunt consequat. Deserunt esse ullamco qui cillum sint magna consequat aliqua in amet.",
+    tech: ["ts", "react"],
+    link: "",
+  },
+  {
+    name: "Urlsito",
+    image: "urlsito.webp",
     description:
       "Pariatur elit occaecat eu sunt ea incididunt amet Lorem occaecat elit et amet pariatur sit. Proident Lorem duis dolore voluptate commodo. Velit et ex nulla labore aliquip ut eu qui dolore in aliqua ad. Mollit dolore et incididunt labore consectetur culpa esse incididunt consequat. Deserunt esse ullamco qui cillum sint magna consequat aliqua in amet.",
     tech: ["ts", "react"],
