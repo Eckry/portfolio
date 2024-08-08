88b52719ea589edfba173a4d4d16dabf0a6b040c
Eckry <112786854+Eckry@users.noreply.github.com>
2024-08-07 18:27:20 -0600

    add initial styles to about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,13 +1,19 @@
-import "./styles/About.css"
+import { IconAbout, IconGame, IconHouse, IconSchool } from "../icons";
+import "./styles/About.css";
 
 export default function About() {
   return (
-    <section>
-      <h1 id="about" className="section-title">More about me</h1>
+    <section className="about-container">
+      <h1 id="about" className="section-title">
+        More about me
+      </h1>
       <div className="bentogrid">
         <div className="grid-section">
-          <h2>Who am i?</h2>
-          <p>
+          <h2 className="about-title">
+            <IconAbout />
+            Who am i?
+          </h2>
+          <p className="about-p">
             Officia non veniam amet nulla enim labore nulla ex occaecat cillum.
             Consequat amet enim commodo deserunt exercitation est labore
             commodo. In est ad quis dolor proident dolor consequat aliquip. Qui
@@ -19,10 +25,31 @@ export default function About() {
           </p>
         </div>
         <div className="grid-section">
-          <h2>How did i learn?</h2>
+          <h2 className="about-title"><IconGame />My hobbies</h2>
+          <p className="about-p">
+            Ipsum exercitation consectetur veniam minim nisi qui adipisicing
+            amet duis. Voluptate laborum irure incididunt tempor laboris
+            incididunt irure mollit dolor consequat voluptate esse irure
+            officia. Laboris proident ea veniam aliqua ex ea.
+          </p>
         </div>
         <div className="grid-section">
-          <h2>I am from</h2>
+          <h2 className="about-title"><IconHouse />Location</h2>
+          <p className="about-p">
+            Ipsum exercitation consectetur veniam minim nisi qui adipisicing
+            amet duis. Voluptate laborum irure incididunt tempor laboris
+            incididunt irure mollit dolor consequat voluptate esse irure
+            officia. Laboris proident ea veniam aliqua ex ea.
+          </p>
+        </div>
+        <div className="grid-section">
+          <h2 className="about-title"><IconSchool />Education</h2>
+          <p className="about-p">
+            Ipsum exercitation consectetur veniam minim nisi qui adipisicing
+            amet duis. Voluptate laborum irure incididunt tempor laboris
+            incididunt irure mollit dolor consequat voluptate esse irure
+            officia. Laboris proident ea veniam aliqua ex ea.
+          </p>
         </div>
       </div>
     </section>
--- src/icons.tsx
+++ src/icons.tsx
@@ -360,3 +360,57 @@ export function IconHTML() {
     </svg>
   );
 }
+
+export function IconGame() {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M12 5m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+      <path d="M20 12.5v4.75a.734 .734 0 0 1 -.055 .325a.704 .704 0 0 1 -.348 .366l-5.462 2.58a5 5 0 0 1 -4.27 0l-5.462 -2.58a.705 .705 0 0 1 -.401 -.691l0 -4.75" />
+      <path d="M4.431 12.216l5.634 -2.332a5.065 5.065 0 0 1 3.87 0l5.634 2.332a.692 .692 0 0 1 .028 1.269l-5.462 2.543a5.064 5.064 0 0 1 -4.27 0l-5.462 -2.543a.691 .691 0 0 1 .028 -1.27z" />
+      <path d="M12 7l0 6" />
+    </svg>
+  );
+}
+
+export function IconHouse() {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M5 12l-2 0l9 -9l9 9l-2 0" />
+      <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" />
+      <path d="M10 12h4v4h-4z" />
+    </svg>
+  );
+}
+
+export function IconSchool() {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M22 9l-10 -4l-10 4l10 4l10 -4v6" />
+      <path d="M6 10.6v5.4a6 3 0 0 0 12 0v-5.4" />
+    </svg>
+  );
+}
