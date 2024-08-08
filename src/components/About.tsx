e368e99b3c82fc1964119b19f984b78043a42622
Eckry <112786854+Eckry@users.noreply.github.com>
2024-08-07 18:52:51 -0600

    add Who am i? text

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,3 +1,4 @@
+import { aboutTexts } from "../consts.d";
 import { IconAbout, IconGame, IconHouse, IconSchool } from "../icons";
 import "./styles/About.css";
 
@@ -13,19 +14,29 @@ export default function About() {
             <IconAbout />
             Who am i?
           </h2>
+          {aboutTexts.who.split("=").map((text, idx) => {
+            if (idx % 2) return <span className="about-highlight">{text}</span>;
+            return <p className="about-p">{text}</p>;
+          })}
+        </div>
+        <div className="grid-section">
+          <h2 className="about-title">
+            <IconGame />
+            My hobbies
+          </h2>
           <p className="about-p">
-            Officia non veniam amet nulla enim labore nulla ex occaecat cillum.
-            Consequat amet enim commodo deserunt exercitation est labore
-            commodo. In est ad quis dolor proident dolor consequat aliquip. Qui
-            irure eu ut elit id duis aliquip dolore ea. Aliquip sunt dolor enim
-            ut occaecat ex magna incididunt dolor. Irure reprehenderit in anim
-            officia deserunt pariatur ipsum dolore adipisicing pariatur
-            exercitation cillum. Voluptate pariatur pariatur nisi eiusmod irure
-            sunt quis aliqua fugiat enim.
+            {aboutTexts.hobbies.split("=").map((text, idx) => {
+              if (idx % 2)
+                return <span className="about-highlight">{text}</span>;
+              return <p className="about-p">{text}</p>;
+            })}
           </p>
         </div>
         <div className="grid-section">
-          <h2 className="about-title"><IconGame />My hobbies</h2>
+          <h2 className="about-title">
+            <IconHouse />
+            Location
+          </h2>
           <p className="about-p">
             Ipsum exercitation consectetur veniam minim nisi qui adipisicing
             amet duis. Voluptate laborum irure incididunt tempor laboris
@@ -34,21 +45,16 @@ export default function About() {
           </p>
         </div>
         <div className="grid-section">
-          <h2 className="about-title"><IconHouse />Location</h2>
+          <h2 className="about-title">
+            <IconSchool />
+            Education
+          </h2>
           <p className="about-p">
-            Ipsum exercitation consectetur veniam minim nisi qui adipisicing
-            amet duis. Voluptate laborum irure incididunt tempor laboris
-            incididunt irure mollit dolor consequat voluptate esse irure
-            officia. Laboris proident ea veniam aliqua ex ea.
-          </p>
-        </div>
-        <div className="grid-section">
-          <h2 className="about-title"><IconSchool />Education</h2>
-          <p className="about-p">
-            Ipsum exercitation consectetur veniam minim nisi qui adipisicing
-            amet duis. Voluptate laborum irure incididunt tempor laboris
-            incididunt irure mollit dolor consequat voluptate esse irure
-            officia. Laboris proident ea veniam aliqua ex ea.
+            {aboutTexts.education.split("=").map((text, idx) => {
+              if (idx % 2)
+                return <span className="about-highlight">{text}</span>;
+              return <p className="about-p">{text}</p>;
+            })}
           </p>
         </div>
       </div>
