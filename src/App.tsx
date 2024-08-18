a261138380f218a96797d47483aeb543bcc18a9d
Eckry <112786854+Eckry@users.noreply.github.com>
2024-08-17 22:31:09 -0600

    add particles to presentation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
+import { useEffect, useMemo, useState } from "react";
 import "./App.css";
 import About from "./components/About";
 import ContactMe from "./components/ContactMe";
 import HeaderBar from "./components/HeaderBar";
 import Presentation from "./components/Presentation";
 import ProjectsSection from "./components/ProjectsSection";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadStarsPreset } from "@tsparticles/preset-stars";
 
 function App() {
+  const [init, setInit] = useState(false);
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadStarsPreset(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const options = useMemo(
+    () => ({
+      preset: "stars",
+      background: { color: "transparent" },
+      fullScreen: { enable: false },
+      particles: { number: { value: 30 }, size: { value: 1 } },
+    }),
+    []
+  );
+
   return (
     <>
       <HeaderBar />
+      {init && <Particles id="tsparticles" options={options} />}
       <main className="main-container">
         <Presentation />
         <ProjectsSection />
