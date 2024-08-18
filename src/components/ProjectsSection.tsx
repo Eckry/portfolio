7f5702e795eb6d2329f2c7562c0b0526ab9d087a
Eckry <112786854+Eckry@users.noreply.github.com>
2024-08-18 10:37:41 -0600

    add title styles

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -1,11 +1,16 @@
 import "./styles/ProjectsSection.css";
-import {projects} from "../consts.d";
+import { projects } from "../consts.d";
 import Project from "./Project";
 
 export default function ProjectsSection() {
   return (
     <section>
-      <h1 id="projects" className="section-title">Projects</h1>
+      <header className="title-header">
+        <h1 id="projects" className="section-title">
+          <span className="title-number">01.</span> Some of my projects
+        </h1>
+        <span className="title-line"></span>
+      </header>
       <div className="projects-container">
         {projects.map((project) => {
           return <Project key={project.name} {...project} />;
