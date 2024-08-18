53c9d1d85a60021bd3bc07e7d8aa8b12860dc3b8
Eckry <112786854+Eckry@users.noreply.github.com>
2024-08-17 21:33:05 -0600

    remove SkillsSection

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import ContactMe from "./components/ContactMe";
 import HeaderBar from "./components/HeaderBar";
 import Presentation from "./components/Presentation";
 import ProjectsSection from "./components/ProjectsSection";
-import SkillsSection from "./components/Skills";
 
 function App() {
   return (
@@ -13,7 +12,6 @@ function App() {
       <main className="main-container">
         <Presentation />
         <ProjectsSection />
-        <SkillsSection />
         <About />
         <ContactMe />
       </main>
